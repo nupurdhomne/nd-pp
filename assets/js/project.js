0cab69e05a580b8005e66ffb750207623e6b6a8d
nupurdhomne <114976909+nupurdhomne@users.noreply.github.com>
2023-02-17 23:26:09 +0530

    Add files via upload

---
+++ assets/js/project.js
@@ -0,0 +1,55 @@
+AOS.init();
+/* Project Cards */
+
+const projectcards = document.querySelector(".projectcards");
+
+
+  
+
+
+// function for rendering project cards data
+const showCards = () => {
+  let output = "";
+  projects.forEach(({ title, cardImage}) => {
+    output += `       
+        <div class="column skill-card card" style="margin: 15px"data-aos="zoom-in-up" data-aos-easing="linear" data-aos-delay="300" data-aos-duration="600" >
+          <div class="wrapper" style="background: url(${cardImage}) center / cover no-repeat;">
+            <div class="header">
+            </div>
+            <div class="data">
+              <div class="content">
+              <div class="title-div">
+                <h1 class="title"><a href="#">${title}</a></h1>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>`;
+  });
+  projectcards.innerHTML = output;
+};
+document.addEventListener("DOMContentLoaded", showCards);
+
+function myFunction() {
+  // Declare variables
+  var input, button, i, skillcard, card, title;
+  input = document.getElementById("myInput").value;
+  input = input.toUpperCase();
+  skillcard = document.getElementsByClassName("skill-card");
+  card = document.getElementsByClassName("card");
+  title = document.getElementsByClassName("title");
+
+  // Loop through all list items, and hide those who don't match the search query
+  for (i = 0; i < button.length; i++) {
+    if (
+      button[i].innerHTML.toUpperCase().includes(input) ||
+      title[i].innerHTML.toUpperCase().includes(input)
+    ) {
+      skillcard[i].style.display = "";
+      card[i].style.display = "";
+    } else {
+      skillcard[i].style.display = "none";
+      card[i].style.display = "none";
+    }
+  }
+}
